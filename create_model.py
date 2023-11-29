345fb9fe1b4b0049c4f91d6b36c78f9520a266f7
Adalberto <multiolx@gmail.com>
2023-11-29 11:01:14 -0300

    Add files via upload

---
+++ create_model.py
@@ -0,0 +1,15 @@
+from modeling_diffbert import DiffBertForDiffusion, DiffBertConfig
+from transformers import AutoTokenizer
+from diffusers import DDIMScheduler
+
+
+
+config = DiffBertConfig(hidden_size=384, num_hidden_layers=2, intermediate_size=768)
+
+model = DiffBertForDiffusion(config)
+tokenizer = AutoTokenizer.from_pretrained("neuralmind/bert-base-portuguese-cased")
+scheduler = DDIMScheduler()
+
+model.save_pretrained("diffbert-mini")
+tokenizer.save_pretrained("diffbert-mini")
+scheduler.save_pretrained("diffbert-mini")
