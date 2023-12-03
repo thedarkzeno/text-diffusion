7320504156309353416f0249606ca552bc73accb
Adalberto <multiolx@gmail.com>
2023-12-03 13:34:39 -0300

    Add files via upload

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python3 train.py --pretrained_model_name_or_path="./diffbert-mini" --dataset_name="Gustavosta/Stable-Diffusion-Prompts" --output_dir="diffbert-mini-trained" 
+python3 train_sample.py --pretrained_model_name_or_path="./diffbert-mini-sample" --dataset_name="cnn_dailymail" --dataset_config_name="3.0.0" --output_dir="diffbert-mini-sample-trained" 
